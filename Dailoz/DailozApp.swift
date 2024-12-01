45ed0d86b4e7eea650eed5ae2d8d88a5446ba154
Nanda WK <nandawinkyu.ix@gmail.com>
2024-12-01 14:06:39 +0700

    Initial Commit

---
+++ Dailoz/DailozApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailozApp.swift
+//  Dailoz
+//
+//  Created by Nanda WK on 2024-12-01.
+//
+
+import SwiftUI
+
+@main
+struct DailozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
